7e036c3f8da4055c1a77639cacdb3655255d0be1
MashaM38 <mvmendeleva38@gmail.com>
2026-06-14 17:30:19 +0300

    Updated init-db-test.js

--- db/init-db-test.js
+++ db/init-db-test.js
@@ -1,6 +1,6 @@
 const sqlite3 = require("sqlite3").verbose();
 
-const db = new sqlite3.Database("../database_test.db");
+const db = new sqlite3.Database("./database_test.db");
 
 module.exports = db;
 
